9ec5b4f0ced1d9770404a175727e48d6415ed096
Paul-HenryN <ngpaulhenry@gmail.com>
2024-06-03 12:00:08 +0200

    chore: setup app structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,16 +5,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthCheckController } from './health-check/health-check.controller';
 import { HealthCheckResolver } from './health-check/health-check.resolver';
+import { UsersResolver } from './users/users.resolver';
+import { UsersModule } from './users/users.module';
+import { MessagesModule } from './messages/messages.module';
+import { ConversationsModule } from './conversations/conversations.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
-      autoSchemaFile: true,
+      autoSchemaFile: 'schema.gql',
     }),
+    UsersModule,
+    MessagesModule,
+    ConversationsModule,
   ],
   controllers: [AppController, HealthCheckController],
-  providers: [AppService, HealthCheckResolver],
+  providers: [AppService, HealthCheckResolver, UsersResolver],
 })
 export class AppModule {}
