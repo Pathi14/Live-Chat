4c64850940642bd0e2ed8e7385d0ad9a06a06b81
Pathi14 <pathipayong@gmail.com>
2024-06-06 16:22:18 +0200

    feat : connect user with database - OK

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,24 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { User } from './models/user.model';
 import { NewUserInput } from './dto/new-user.input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
 
 export const users: User[] = [];
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   async addUser(data: NewUserInput): Promise<User> {
-    const user: User = {
-      ...data,
-      id: `user#${users.length + 1}`,
-      creationDate: new Date(),
-    };
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    return this.prisma.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+  }
 
-    users.push(user);
-
-    return user;
+  async getUserByUsername(username: string): Promise<User | null> {
+    return this.prisma.user.findUnique({ where: { username } });
   }
 
   async getUsers(): Promise<User[]> {
-    return users;
+    return this.prisma.user.findMany();
+  }
+
+  async validateUser(username: string, pass: string): Promise<User | null> {
+    const user = await this.getUserByUsername(username);
+    if (user && await bcrypt.compare(pass, user.password)) {
+      return user;
+    }
+    return null;
   }
 }
